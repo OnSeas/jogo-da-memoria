10e486dbfc88099edfd682a4dbc8ca7e09e8c555
Osmar Neto <osmarneto750@gmail.com>
2020-10-09 12:22:14 -0300

    Adicionando função para verificar se cartas são iguais

--- js/main.js
+++ js/main.js
@@ -1,13 +1,47 @@
 /* Feito por Osmar Januario de souza Neto */
 
 const cards = document.querySelectorAll('.card');
+let HasFlippedCard = false;
+let FirstCard, SecondCard;
 
-// Função que vira as cartas.
-
-function FlipCard() {
-    this.classList.toggle('flip'); // Adiciona a classe flip para o elemento que chamar a função.
+// Função que desabilita cards.
+function DisableCards() {
+    
 }
 
+// Função que Desvira cards.
+function UnflipCards() {
+
+}
+
+// Função que verifica se há match entre as cartas.
+function CheckForMatch() {
+
+    if(FirstCard.dataset.card === SecondCard.dataset.card) { // Verifica se as cartas são iguais.
+        DisableCards();
+        return;
+    }
+
+    UnflipCards();
+}
+
+// Função que vira as cartas.
+function FlipCard() {
+
+    this.classList.add('flip'); // Adiciona a classe flip para o elemento que chamar a função.
+
+    if(!HasFlippedCard) { // Verificando se é a primeira carta a ser clicada.
+        HasFlippedCard = true;
+        FirstCard = this;
+        return; // termina a função.
+    }
+
+    SecondCard = this; // Só chega até aqui se for a segunda carta clicada.
+    CheckForMatch();
+    HasFlippedCard = false;
+}
+
+// Percorre o array cards que contém todos os cards vendo qual foi clicado.
 cards.forEach((card) => {
-    card.addEventListener('click', FlipCard)
+    card.addEventListener('click', FlipCard);
 })
